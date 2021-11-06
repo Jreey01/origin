10a7442968e8967ef1a7cb5536d1ff28ccee5b36
elwin <nwlhb@163.com>
2021-11-07 05:01:55 +0800

    删除备注

--- learnpython/workproject/learnpy.py
+++ learnpython/workproject/learnpy.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 # -*-coding:utf-8 -*-
 # @Time    :2021/10/26:17:26
-# @Author   :ELWIN
-# @Email    :nwlhb@163.com
 # @File     :learnpy.py
 import requests
 from selenium import webdriver
